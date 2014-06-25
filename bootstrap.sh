2973d198a43ae846b6424288dda8feedf7905a19
Johan Henkens <jhenkens@users.noreply.github.com>
2014-06-25 12:45:26 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,6 @@ libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev \
 libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev \
 libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev \
 libcurl3-dev imagemagick libmagickwand-dev libpcre3-dev vim git-core \
-postgresql-9.1 postgresql-server-dev-9.1 nodejs
+postgresql-9.1 postgresql-server-dev-9.1 nodejs python-software-properties
 
 su -c 'bash /vagrant/bootstrap-vagrant.sh' vagrant
